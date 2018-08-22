822f009662506add74c51c805c9425033bfee2c4
luis <yongchun1683@gmail.com>
2018-08-22 12:52:27 +0800

    add loading gif and clear input field on loan calculator

--- 002-loanCalculator/app.js
+++ 002-loanCalculator/app.js
@@ -1,7 +1,15 @@
 //! Listen for submit
-document.getElementById('loan-form').addEventListener('submit', calculateResults);
+document.getElementById('loan-form').addEventListener('submit', function(e) {
+    document.getElementById('results').style.display = 'none'
+    document.getElementById('loading').style.display = 'block'
 
-function calculateResults(e) {
+    setTimeout(calculateResults, 1500);
+    e.preventDefault();
+});
+
+document.getElementById('clear').addEventListener('click', clearInput);
+
+function calculateResults() {
     //* Define UI variables
     const amount = document.getElementById('amount');
     const interest = document.getElementById('interest');
@@ -21,14 +29,19 @@ function calculateResults(e) {
         monthlyPayment.value = monthly.toFixed(2);
         totalPayment.value = (monthly * calculatedPayments).toFixed(2);
         totalInterest.value = ((monthly * calculatedPayments) - principal).toFixed(2);
+
+        document.getElementById('results').style.display = 'block';
+        document.getElementById('loading').style.display = 'none';
     } else {
         showError('Please check your number input!');
     }
 
-    e.preventDefault();
 }
 
 function showError(error) {
+    document.getElementById('results').style.display = 'none';
+    document.getElementById('loading').style.display = 'none';
+
     const errorDiv  = document.createElement('div');
 
     const card = document.querySelector('.card');
@@ -40,10 +53,23 @@ function showError(error) {
 
     card.insertBefore(errorDiv, heading);
 
-    //* clear error after 1.5 seconds
-    setTimeout(clearError, 1500);
+    //* clear error after 2 seconds
+    setTimeout(clearError, 2000);
 }
 
 function clearError() {
     document.querySelector('.alert').remove();
+}
+
+//! Clear all the input
+function clearInput() {
+    const amount = document.getElementById('amount');
+    const interest = document.getElementById('interest');
+    const years = document.getElementById('years');
+
+    amount.value = '';
+    interest.value = '';
+    years.value = '';
+
+    document.getElementById('results').style.display = 'none';
 }
